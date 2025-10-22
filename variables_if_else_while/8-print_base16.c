f758278f8417a10bac8c182b752ae8521549165e
nobulp <lucaslupon873@gmail.com>
2025-10-22 13:44:21 +0000

    guvsqugcvus

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - Prints all the numbers of base 16 in lowercase
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int i;
-    char j;
+int i;
+char j;
 
-    for (i = 0; i < 10; i++)
-        putchar(i + '0');
+for (i = 0; i < 10; i++)
+putchar(i + '0');
 
-    for (j = 'a'; j <= 'f'; j++)
-        putchar(j);
+for (j = 'a'; j <= 'f'; j++)
+putchar(j);
 
-    putchar('\n');
+putchar('\n');
 
-    return (0);
+return (0);
 }

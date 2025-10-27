2e8dc9db65cbc159983e5266f450b6e3bc0fa9ff
nobulp <lucaslupon873@gmail.com>
2025-10-27 14:46:13 +0000

    feat add a script that multiply a and b

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplie deux entiers
+ * @a: Le premier entier
+ * @b: Le second entier
+ *
+ * Return: Le résultat de la multiplication
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}

ee52c3d67a0f261271271972407cc9ac9c6df26f
nobulp <lucaslupon873@gmail.com>
2025-10-27 13:31:34 +0000

    feat: Add a script that return 1 if c is uppercase 0 if don't

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}

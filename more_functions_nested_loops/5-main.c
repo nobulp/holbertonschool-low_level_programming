6c21be5be513ab162c53a92907c48a72d55a43bd
nobulp <lucaslupon873@gmail.com>
2025-10-27 17:01:54 +0000

    feat: Add a script that prints 10 times the numbers, from 0 to 14, followed by a new line.

---
+++ more_functions_nested_loops/5-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - test de la fonction more_numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+more_numbers();
+return (0);
+}
---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - imprime 10 fois les nombres de 0 à 14
+ *
+ * Return: rien
+ */
+void more_numbers(void)
+{
+int i, j;
+
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j >= 10)
+_putchar('0' + j / 10);
+_putchar('0' + j % 10);
+}
+_putchar('\n');
+}
+}

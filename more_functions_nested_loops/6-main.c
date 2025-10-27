2b8e7224170d02938170ee997207d53c21736ea6
nobulp <lucaslupon873@gmail.com>
2025-10-27 17:09:27 +0000

    feat add a function that draws a straight line in the terminal.

---
+++ more_functions_nested_loops/6-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test la fonction print_line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_line(0);
+print_line(2);
+print_line(10);
+print_line(-4);
+return (0);
+}
---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - dessine une ligne droite avec le caractère '_'
+ * @n: nombre de fois que le caractère '_' doit être imprimé
+ *
+ * Return: rien
+ */
+void print_line(int n)
+{
+int i;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}

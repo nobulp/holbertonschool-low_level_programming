2c57c3c0951750d26f905aa8aaac7d6f24ad786a
nobulp <lucaslupon873@gmail.com>
2025-10-27 14:02:22 +0000

    fix

--- more_functions_nested_loops/0-main.c
+++ more_functions_nested_loops/0-main.c
@@ -6,12 +6,12 @@
  *
  * Return: Always 0.
  */
- int main(void)
+int main(void)
 {
 char c;
 c = 'A';
 printf("%c: %d\n", c, _isupper(c));
-c= 'a';
+c = 'a';
 printf("%c: %d\n", c, _isupper(c));
 return (0);
 }

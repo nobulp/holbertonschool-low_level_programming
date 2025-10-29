3561b6a134f5a272d6d0f24eb311de7459aa40d0
nobulp <lucaslupon873@gmail.com>
2025-10-29 08:34:50 +0000

    feat: Add a script that concatenates two strings.

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - concatène deux chaînes de caractères
+ * @dest: chaîne de destination
+ * @src: chaîne source
+ *
+ * Return: pointeur vers la chaîne résultante dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int j = 0;
+
+
+while (dest[i] != '\0')
+{
+i++;
+}
+
+
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+
+dest[i] = '\0'; 
+return (dest);
+}

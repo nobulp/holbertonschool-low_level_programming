bffa2034a69c55280847b1233aea122ac87f3a71
nobulp <lucaslupon873@gmail.com>
2025-11-04 23:17:52 +0000

    feat: Add a function that prints the chessboard.

--- pointers_arrays_strings/_putchar.c
+++ pointers_arrays_strings/_putchar.c
@@ -1,13 +1,12 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar - écrit le caractère c sur la sortie standard
+ * @c: Le caractère à afficher
  *
- * Return: On success 1.
- * On error, -1 is returned and errno is set appropriately.
+ * Return: 1 en cas de succès, -1 en cas d’erreur
  */
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+    return (write(1, &c, 1));
 }

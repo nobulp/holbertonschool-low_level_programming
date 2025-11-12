d5fc87f60314792ebdfa9b801a856b0bbf4c02f9
nobulp <lucaslupon873@gmail.com>
2025-11-12 21:39:05 +0000

    fix: add a function that creates an array of chars, and initializes it with a specific char.

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars, and initializes it with a specific char
+ * create_array - creates an array of chars and initializes it with a char
  * @size: size of the array
  * @c: char to initialize the array with
  *
